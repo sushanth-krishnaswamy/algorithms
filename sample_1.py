8d5cae75f0d1ec0e39fa1b280d8e1b7ed1123fef
Sushanth Krishnaswamy <sushanth.k4@gmail.com>
2016-12-08 18:43:42 -0500

    first test file

---
+++ sample_1.py
@@ -0,0 +1,10 @@
+x = input("enter the value of x : ")
+y = input("enter the value of y : ")
+
+
+temp =x
+x=y
+y=temp
+
+print("the value of x after swapping is : ",x)
+print("the value of y after swapping is : ",y)
