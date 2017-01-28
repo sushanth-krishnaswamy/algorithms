7722532aec7971c8b7cc4b6f910e023273a00b58
sushanth-krishnaswamy <sushanth.k4@gmail.com>
2017-01-28 10:55:21 -0500

    made changes to the binary tree implementation

--- github-java stuff/src/ctcr_codes/BinaryTree.java
+++ github-java stuff/src/ctcr_codes/BinaryTree.java
@@ -84,10 +84,74 @@ class BinaryTreeImpl{
 		}
 	}
 	
+	public boolean search(int val){
+		return search(root, val);
+	}
 	
+	private boolean search(TreeNode r, int val){
+		if(r.getData()==val){
+			return true;
+		}
+		if(r.getLeft()!=null){
+			if(search(r.getLeft(),val)){
+				return true;
+			}
+		}
+		if(r.getRight()!=null){
+			if(search(r.getRight(),val)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void inorder(){
+		inorder(root);
+	}
+	
+	private void inorder(TreeNode r){
+		if(r!=null){
+			inorder(r.getLeft());
+			System.out.println(r.getData()+" ");
+			inorder(r.getRight());
+		}
+	}
+	
+	public void preorder(){
+		preorder(root);
+	}
+	
+	private void preorder(TreeNode r){
+		if(r!=null){
+			System.out.println(r.getData()+" ");
+			preorder(r.getLeft());
+			preorder(r.getRight());
+		}
+	}
+	
+	public void postorder(){
+		postorder(root);
+	}
+	
+	private void postorder(TreeNode r){
+		if(r!=null){
+			postorder(r.getLeft());
+			postorder(r.getRight());
+			System.out.println(r.getData()+" ");
+		}
+	}
 	
 }
 
 public class BinaryTree {
 
+	public static void main(String[] args){
+		BinaryTreeImpl bt = new BinaryTreeImpl();
+		
+		System.out.println("testing binary tree\n");
+		
+		/*do{
+			
+		}*/
+	}
 }
